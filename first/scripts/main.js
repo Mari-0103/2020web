cc91433cb8d9d449bba6bbe39aea808d6020f458
Mari-0103 <75403363+Mari-0103@users.noreply.github.com>
2020-12-10 12:19:38 +0900

    Update main.js

--- first/scripts/main.js
+++ first/scripts/main.js
@@ -8,12 +8,11 @@
       
 }
     
-    let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   setUserName();
 }
-
+    let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
